71fa3599ce39324c4e514a07c9a67fc29bd579f7
James White <cyberyt@gmail.com>
2015-08-27 19:21:37 +0100

    FML to_sym does what I want

--- mta.rb
+++ mta.rb
@@ -5,7 +5,7 @@ mta = {
 }
 
 puts "Welcome to MTA! \n Which line are you getting on at?"
-line_on = gets.chomp.downcase
+line_on = gets.downcase.chomp.to_sym
 puts line_on
 
 puts "Which Station are you getting on at?"
@@ -13,8 +13,20 @@ station_on = gets.chomp.downcase
 puts station_on
 
 puts "Which line is your destination station on?"
-line_off = gets.chomp.downcase 
+line_off = gets.chomp.downcase.to_sym
 
 puts "Which station are you going to?"
 station_off = gets.chomp.downcase
 
+# index_on = mta[line_on].index(station_on)
+# index_off = mta[line_off].index(station_off)
+
+# if line_on == line_off
+#   stops = (index_on - index_off).abs
+# end
+
+if line_on == line_off
+  stops = (mta[line_on].index(station_on) - mta[line_off].index(station_off)).abs
+end
+
+puts stops
